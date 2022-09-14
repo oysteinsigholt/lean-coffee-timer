0854b092586fac7abc2c593c6bec8f42c94e209d
Øystein Sigholt <oystein.sigholt@gmail.com>
2022-09-14 23:08:07 +0200

    add button style to getting started button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,8 @@
 	export const prerender = true;
 </script>
 
-<a href="/lean-coffee-timer/settings">Get started</a>
+<a
+	href="/lean-coffee-timer/settings"
+	class="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 bg-slate-900 text-white hover:bg-slate-700"
+	>Get started</a
+>
